dd7df56159f121b8d0a1bfe52ae045f3d848a27c
Axlfc <axel.fernandez@mercatsdetarragona.cat>
2021-10-14 22:11:31 +0200

    UPDATE: need more changes to root_programs

--- script.py
+++ script.py
@@ -5,9 +5,11 @@ try:
     first_line = o.readline().split("features")
     num_user_features = int(filter(str.isdigit, first_line[0]))
     num_root_features = int(filter(str.isdigit, first_line[1]))
+    num_total_features = int(filter(str.isdigit, first_line[2]))
     lines = o.readlines()[4:]
     #print(lines)
     feature_list = []
+    root_feature_list = []
     for i in range(num_user_features):
         feature_list.append(lines[i].split("|"))
     with open('user_programs', 'w') as w:
@@ -17,12 +19,20 @@ try:
             if k == num_user_features:
                 break
             k += 1
-    with open('root_programs', 'w') as w:
-        k = 0
-        while k <= len(feature_list) - 2:
-            w.write(feature_list[k][1] + '\n')
-            if k == num_user_features:
-                break
-            k += 1
+        w.close()
+
+    with open('root_programs', 'w') as x:
+        k = num_user_features + 1
+        #print("skip lines here to trim data...")
+        h = 0
+        for j in range(k, num_total_features):
+            print(h)
+            thing = lines[h].split("|")
+            print(thing[1])
+            root_feature_list.append(thing)
+            #print(thing[1])
+            x.write(thing[h] + '\n')
+            h += 1
+        x.close()
 finally:
     o.close()
